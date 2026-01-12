6f5500f828dd2c6bf1ef0a12ae51d2d334750b8a
ChrisVN123 <christianvanell@icloud.com>
2026-01-12 16:41:03 +0100

    Co-authored-by: ElMiho <ElMiho@users.noreply.github.com>

--- week2/vanell/exercise2_3.jl
+++ week2/vanell/exercise2_3.jl
@@ -8,58 +8,62 @@ using Polynomials
 include("exercise2_1.jl")
 include("exercise2_2.jl")
 
+println("EXERCISE 2.3")
+
+function k_rs(r, s, lam1, lam2)
+    
+end 
+
 function assembly_matrix(VX, VY, EToV, lam1, lam2, qt)
     N = size(EToV)[1]
+    M = length(VX)
     
-    A = Matrix{Float64}(undef, N, N)
-    b = Matrix{Float64}(undef, N, 1)
-    
+    A = zeros(Float64, M, M)
+    b = zeros(Float64, M)  # vector is simpler/safer
+
     for n in 1:N
         i, j, k = EToV[n, :]
-        x_i, y_i = VX[i], VY[i]
-        x_j, y_j = VX[j], VY[j]
-        x_k, y_k = VX[k], VY[k]
+        Δ, abc = basfun(n, VX, VY, EToV)        
         
         for r in 1:3
-            Δ, abc = basfun(n, VX, VY, EToV)
-            q̃ = (qt(x_i, y_1) + qt(x_j, y_j) + qt(x_k, y_k)) / 3 
-            q_r = abs(Δ)/3 * q̃
+            q̃ = (qt[i] + qt[j] + qt[k]) / 3 
+            q_r_n = abs(Δ)/3 * q̃
+            ĩ = EToV[n, r]
             
-            ĩ = 0 
-            if r == 1 
-                ĩ = i 
-            elseif r == 2 
-                ĩ = j 
-            elseif r == 3 
-                ĩ = k 
-            end 
-            
-            b[ĩ] += q_r
+            b[ĩ] += q_r_n
             for s = 1:3
-                j̃ = 0 
-                if r == 1 
-                    j̃ = i 
-                elseif r == 2 
-                    j̃ = j 
-                elseif r == 3 
-                    j̃ = k 
-                end 
+                j̃ = EToV[n, s]
 
+                b_r, b_s = abc[r, 2], abc[s, 2]
+                c_r, c_s = abc[r, 3], abc[s, 3]
+                
                 k_rs = 1 / (4 * abs(Δ)) * (lam1 * b_r * b_s + lam2 * c_r * c_s)
+                A[ĩ, j̃] += k_rs
             end 
         end 
-
-
-        q(n) = nothing
-        b[i] := b[i] + q(n)
-    
-            for s in 1:3
-                #Compute k(n)r,s
-                if j ≥ i
-                    #Look up the global numbers (i,j) and (x,y)-coordinates of the nodes in en.
-                    a[i,j] := a[i,j] + k(n)r,s
-                else
-                    a[j,i] := a[j,i] + k(n)
-                end
-            end
     end
+
+    return A, b 
+end 
+
+println("CASE 1")
+x0, y0 = 0, 0 
+L1, L2 = 1, 1
+noelms1, noelms2 = 4, 3 
+qt = zeros(noelms1 * noelms2 * 2)
+lam1 = 1 
+lam2 = 1
+
+VX, VY = xy(x0, y0, L1, L2, noelms1, noelms2)
+EToV = conelmtab(noelms1, noelms2)
+
+A, b = assembly_matrix(VX, VY, EToV, lam1, lam2, qt)
+display(A)
+display(diag(A))
+#display(solve)
+
+# function test_q(x,y)
+#     return -6*x + 2*y - 2
+# end
+
+# qt_space = test_q.(VX,VY)
